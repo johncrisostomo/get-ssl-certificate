c03cc7e3505392746bc71223f9e5244f2f72ae47
Preston Lamb <preston.j.lamb@gmail.com.com>
2017-12-13 10:45:00 -0700

    Added a pem encoding function from the certificate.raw.toString(base64) on the peer certificate

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
 var https = require('https');
 
 function isEmpty(object) {
-  for(var prop in object) {
-    if(object.hasOwnProperty(prop))
-      return false;
+  for (var prop in object) {
+    if (object.hasOwnProperty(prop)) return false;
   }
 
   return true;
 }
 
+function pemEncode(str, n) {
+  const ret = [];
+
+  for (let i = 1; i <= str.length; i++) {
+    ret.push(str[i - 1]);
+    const mod = i % n;
+
+    if (mod === 0) {
+      ret.push('\n');
+    }
+  }
+  const returnString = `-----BEGIN CERTIFICATE-----
+${ret.join('')}-----END CERTIFICATE-----`;
+  return returnString;
+}
+
 function get(url) {
   if (url.length <= 0 || typeof url !== 'string') {
-    throw Error("A valid URL is required");
+    throw Error('A valid URL is required');
   }
 
   var options = {
-    hostname: url, 
-    agent: false, 
+    hostname: url,
+    agent: false,
     rejectUnauthorized: false,
-    ciphers: "ALL",
+    ciphers: 'ALL',
   };
-  
-  return new Promise(function (resolve, reject) {
-    var req = https.get(options, function (res) {
+
+  return new Promise(function(resolve, reject) {
+    var req = https.get(options, function(res) {
       var certificate = res.socket.getPeerCertificate();
-      if(isEmpty(certificate) || certificate === null) {
-        reject({message: 'The website did not provide a certificate'});
+      if (isEmpty(certificate) || certificate === null) {
+        reject({ message: 'The website did not provide a certificate' });
       } else {
+        if (certificate.raw) {
+          certificate.pemEncoded = pemEncode(certificate.raw.toString('base64'), 64);
+        }
         resolve(certificate);
       }
     });
