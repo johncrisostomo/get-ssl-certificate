9a2518a2cd2e1cdce6f97571b0e9fa5c6e3d07ad
John Crisostomo <john.crisostomo@icloud.com>
2018-09-17 14:50:53 +0800

    Add options object

--- index.js
+++ index.js
@@ -1,63 +1,73 @@
-var https = require('https')
+var https = require('https');
 
-function isEmpty(object) {
-  for (var prop in object) {
-    if (object.hasOwnProperty(prop)) return false
-  }
-
-  return true
-}
-
-function pemEncode(str, n) {
-  var ret = []
-
-  for (var i = 1; i <= str.length; i++) {
-    ret.push(str[i - 1])
-    var mod = i % n
-
-    if (mod === 0) {
-      ret.push('\n')
-    }
-  }
-
-  var returnString = `-----BEGIN CERTIFICATE-----\n${ret.join('')}\n-----END CERTIFICATE-----`
-
-  return returnString
-}
-
-function get(url) {
-  if (url.length <= 0 || typeof url !== 'string') {
-    throw Error('A valid URL is required')
-  }
-
-  var options = {
+function getOptions(url, options) {
+  var defaultOptions = {
     hostname: url,
     agent: false,
     rejectUnauthorized: false,
+    timeout: 5000,
     ciphers: 'ALL'
+  };
+
+  return {
+    ...defaultOptions,
+    ...options
+  };
+}
+
+function isEmpty(object) {
+  for (var prop in object) {
+    if (object.hasOwnProperty(prop)) return false;
   }
+
+  return true;
+}
+
+function pemEncode(str, n) {
+  var ret = [];
+
+  for (var i = 1; i <= str.length; i++) {
+    ret.push(str[i - 1]);
+    var mod = i % n;
+
+    if (mod === 0) {
+      ret.push('\n');
+    }
+  }
+
+  var returnString = `-----BEGIN CERTIFICATE-----\n${ret.join('')}\n-----END CERTIFICATE-----`;
+
+  return returnString;
+}
+
+function get(url, options) {
+  if (url.length <= 0 || typeof url !== 'string') {
+    throw Error('A valid URL is required');
+  }
+
+  var options = getOptions(url, options);
 
   return new Promise(function(resolve, reject) {
     var req = https.get(options, function(res) {
-      var certificate = res.socket.getPeerCertificate()
+      var certificate = res.socket.getPeerCertificate();
       if (isEmpty(certificate) || certificate === null) {
-        reject({ message: 'The website did not provide a certificate' })
+        reject({ message: 'The website did not provide a certificate' });
       } else {
         if (certificate.raw) {
-          certificate.pemEncoded = pemEncode(certificate.raw.toString('base64'), 64)
+          certificate.pemEncoded = pemEncode(certificate.raw.toString('base64'), 64);
         }
-        resolve(certificate)
+        resolve(certificate);
       }
-    })
+    });
 
     req.on('error', function(e) {
-      reject(e)
-    })
+      reject(e);
+    });
 
-    req.end()
-  })
+    req.end();
+  });
 }
 
 module.exports = {
   get: get
-}
+};
